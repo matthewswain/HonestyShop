80465996a857940cd78fa9b1f0ce81d1a6265344
Matthew Swain <matthewswain@gmail.com>
2014-05-12 21:15:13 +0100

    Added working user model with salting, password hashing and authentication.

--- models.py
+++ models.py
@@ -1,22 +1,56 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey 
 from sqlalchemy.orm import relationship, backref
 from database import Base
+from random import choice
+from string import ascii_uppercase, ascii_lowercase, digits
+from hashlib import sha512
+
+class Security:
+
+
+    @staticmethod
+    def make_salt():
+
+        salt = ''
+
+        for i in range(0, 10):
+            salt += choice(ascii_uppercase + ascii_lowercase + digits)
+
+        return salt
+
+
+    @staticmethod
+    def salt_and_hash(password, salt):
+        return sha512(password + salt).hexdigest()
+
+    @staticmethod
+    def authenticate(user, password):
+
+        if user.password == Security.salt_and_hash(password, user.salt):
+            return True
+        else:
+            return False
 
 
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
+    email = Column(String, unique=True, nullable=False)
+    salt = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    name = Column(String)
     transactions = relationship('Transaction', backref='user')
+    
 
-
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, email, password):
+        self.email = email
+        self.salt = Security.make_salt()
+        self.password = Security.salt_and_hash(password, self.salt)
 
 
     def __repr__(self):
-        return '<User {0} - {1}>'.format(self.id, self.name)
+        return '<User {0} - {1}>'.format(self.id, self.email)
 
 
 class Item(Base):
